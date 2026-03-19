7d6566b1c38050c928d9a062c93043c151cfc925
Prateek-Lohani <prateeklohani105@gmail.com>
2026-03-19 17:22:22 +0530

    text color fixes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -118,7 +118,7 @@ const Demo = () => {
         {isFetching ? (
           <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
+          <p className='font-inter font-bold text-white text-center'>
             Well, that wasn't supposed to happen...
             <br />
             <span className='font-satoshi font-normal text-gray-300'>
